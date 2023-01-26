cd018ff840fd8063bdf85722d6642264156c0470
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2023-01-26 22:28:50 +0530

    Time: 22 ms (34.62%), Space: 13.6 MB (13.29%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -4,10 +4,11 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        digits = [int(digit) for digit in str(num)]
-        count = 0
-        for digit in digits:
-            if num % digit == 0:
-                count += 1
-        return count
+        # digits = [int(digit) for digit in str(num)]
+        # count = 0
+        # for digit in digits:
+        #     if num % digit == 0:
+        #         count += 1
+        # return count
+        return sum(1 if num % int(d) == 0 else 0 for d in str(num))
         
