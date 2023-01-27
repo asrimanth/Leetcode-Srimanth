6db85d31095890dbdbae6044ed4ad58a2dfed54f
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2023-01-27 19:50:47 +0530

    Time: 136 ms (53.54%), Space: 15.6 MB (82.66%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: int
         """
         # Use XOR
+        # Explanation: https://leetcode.com/problems/single-number/discuss/2778969/Python-XOR-Solution-Fully-Explained-(O(n)-time-O(1)-space)
         result = 0
         for num in nums:
             result ^= num
