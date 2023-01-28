382fde799b930dc1f64aaccd311be64592251d05
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2023-01-28 22:29:04 +0530

    Time: 15 ms (86.60%), Space: 13.3 MB (66.23%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        triangle = [[1]]
+        for i in range(1, rowIndex+1):
+            binomial_row = [triangle[i-1][0]]
+            for j in range(i-1):
+                binomial_row.append(triangle[i-1][j] + triangle[i-1][j+1])
+            binomial_row.append(1)
+            triangle.append(binomial_row)
+        return triangle[-1]
