a114cde6d6fa1a73dfb5e256178a850344add824
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2023-02-03 22:43:44 -0500

    Time: 36 ms (34.09%), Space: 13.7 MB (15.78%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if head is None:
+            return head
+
+        current_node = head
+
+        while current_node.next:
+            if current_node.val == current_node.next.val:
+                current_node.next = current_node.next.next
+            else:
+                current_node = current_node.next
+        return head
