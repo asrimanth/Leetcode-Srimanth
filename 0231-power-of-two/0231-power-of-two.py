cb21b27db270a568d0ce3d553c341cfb32a1fbda
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-01-23 12:32:12 -0500

    Time: 36 ms (73.39%), Space: 16.4 MB (58.95%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,8 +1,6 @@
-class Solution(object):
-    def isPowerOfTwo(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        # Bit manipulation: https://leetcode.com/problems/power-of-two/discuss/948641/Python-O(1)-Solution
-        return n>0 and (n & n-1 == 0)
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n==0:
+            return False
+        else:
+            return (n & n-1)==0
