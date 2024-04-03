412ef3a3fa786f2485a1239e7b7181e1f00745f1
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-04-03 17:45:24 -0400

    Time: 35 ms (76.42%), Space: 16.7 MB (10.15%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,4 +1,5 @@
 # Time complexity: O(nlogn)
+# Use quick-select to bring the complexity down to O(2n)
 # Space complexity: O(1)
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
