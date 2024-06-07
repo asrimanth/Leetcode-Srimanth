976af7c244471f0eaddc440498ea319a4e8fb105
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-06-07 00:31:59 -0400

    Time: 64 ms (47.7%), Space: 17.7 MB (54.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,8 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Hash map approach
         hash_map = dict()
 
         for i, num in enumerate(nums):
