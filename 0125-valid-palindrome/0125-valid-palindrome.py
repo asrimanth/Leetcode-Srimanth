cfac1e764b6be602fb91f26585b8806762929405
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-19 16:59:52 -0400

    Time: 50 ms (30.31%), Space: 17 MB (52.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,15 @@
-# Two pointer approach
-# Time Complexity: O(n)
-# Space Complexity: O(1)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left, right = 0, len(s)-1
-        s = s.lower()
-        while left < right:
-            if not s[left].isalnum():
-                left += 1
-            elif not s[right].isalnum():
-                right -= 1
-            elif s[left] != s[right]:
-                return False
+        # Two pointers approach
+        i, j = 0, len(s)-1
+        while i<j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            elif s[i].lower() != s[j].lower():
+                    return False
             else:
-                left += 1
-                right -= 1
+                i += 1
+                j -= 1
         return True
