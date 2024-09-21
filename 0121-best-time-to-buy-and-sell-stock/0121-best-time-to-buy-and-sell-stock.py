2c95123f71533fdf2eb79ceccab01a39265b8034
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-20 22:13:05 -0400

    Time: 780 ms (23.94%), Space: 27.5 MB (38.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,16 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+# Two pointer approach
+# https://www.youtube.com/watch?v=1pkOgXD63yU
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         left, right = 0, 1
         max_profit = 0
         while right < len(prices):
             if prices[right] > prices[left]:
-                profit = prices[right] - prices[left]
-                max_profit = max(max_profit, profit)
+                # We are buying low and selling high
+                curr_profit = prices[right] - prices[left]
+                max_profit = max(max_profit, curr_profit)
             else:
                 left = right
             right += 1
