ca134a218594eaccc7b70aa54f9696545db04eb0
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-23 13:22:48 -0400

    Time: 56 ms (47.93%), Space: 16.7 MB (28.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,27 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        carry = 0
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         result = ListNode()
         out = result
+        carry = 0
         while l1 or l2:
-            if l1: out.val += l1.val
-            if l2: out.val += l2.val
-            carry = out.val // 10
-            out.val = out.val % 10
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            val = val1 + val2 + carry
+
+            # New digit and carry
+            carry = val // 10
+            val %= 10 # Units place only
+            out.next = ListNode(val)
+
+            # Update pointers
+            out = out.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            if (carry != 0) or l1 or l2:
-                out.next = ListNode(carry)
-                out = out.next
 
-        return result
+        if carry != 0:
+            out.next = ListNode(carry)
+            out = out.next
+        return result.next
