23b054e785c65c383fb47888ec312586d915d698
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-23 13:04:46 -0400

    Time: 26 ms (98.1%), Space: 17.8 MB (28.38%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,19 +1,23 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        new_ll = None
-        current = head
-        while current is not None:
-            next_node = current.next
-            current.next = new_ll
-            new_ll = current
-            current = next_node
-        return new_ll
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.reverse_list_iter(head)
+    
+    def reverse_list_iter(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Approach: Save node.next pointer in a temp
+        # Then break node.next and assign it to previous node.
+        # Hence, we reverse the connections as we move along.
+        prev, curr = None, head
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
