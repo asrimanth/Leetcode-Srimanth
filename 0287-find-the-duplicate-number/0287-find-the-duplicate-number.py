aeb5540af746c6af03b23c648db9efd1773158eb
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-24 17:05:10 -0400

    Time: 485 ms (28.74%), Space: 30.8 MB (31.69%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,8 +1,25 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        return self.set_method(nums)
+        return self.rabbit_and_hare_method(nums)
     
-    
+    def rabbit_and_hare_method(self, nums: List[int]) -> int:
+        # Useful for cycle detection in linked lists
+        # https://www.youtube.com/watch?v=wjYnzkAhcNk
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow] # Advance slow once
+            fast = nums[nums[fast]] # Advance fast twice
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+        
+
     def set_method(self, nums: List[int]) -> int:
         # Time Complexity: O(N)
         # Space Complexity: O(N)
