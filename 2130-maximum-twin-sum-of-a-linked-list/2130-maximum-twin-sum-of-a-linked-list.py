fb34ebd871b7dc5c450e91cfc2b10c3999765de3
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-29 23:57:58 -0400

    Time: 410 ms (9.17%), Space: 45.8 MB (6.73%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -8,6 +8,23 @@
 from copy import deepcopy
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+        # return self.method_1(head)
+        return self.stack_method(head)
+    
+    def stack_method(self, head: Optional[ListNode]) -> int:
+        temp = head
+        stack = []
+        while temp:
+            stack.append(temp.val)
+            temp = temp.next
+        
+        max_sum = -1
+        while head:
+            max_sum = max(head.val + stack.pop(), max_sum)
+            head = head.next
+        return max_sum
+    
+    def method_1(self, head: Optional[ListNode]) -> int:
         rev = self.reverse(deepcopy(head))
         max_sum = -1
         while head:
