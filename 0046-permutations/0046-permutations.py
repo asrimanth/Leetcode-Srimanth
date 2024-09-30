3d20bd14f0194bdc68d53250b84510a683cc562e
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-29 21:56:44 -0400

    Time: 47 ms (13.86%), Space: 16.9 MB (7.93%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -7,15 +7,14 @@ class Solution:
         n = len(nums)
         result = set()
         def dfs(i):
-            if i == n:
+            if i == n-1:
+                result.add(tuple(nums.copy()))
                 return
             for j in range(i+1, n):
                 nums[i], nums[j] = nums[j], nums[i]
-                result.add(tuple(nums.copy()))
                 dfs(i+1)
                 # Backtrack => swap to original
                 nums[i], nums[j] = nums[j], nums[i]
-                result.add(tuple(nums.copy()))
                 dfs(i+1)
         dfs(0)
         return list(result)
