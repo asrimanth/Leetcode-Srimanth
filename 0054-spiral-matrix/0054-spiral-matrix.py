d94b15d23593d601f72dc3cbad6e3713d7629ed3
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-09-30 00:52:51 -0400

    Time: 40 ms (20.44%), Space: 16.6 MB (10.35%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,42 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        if not matrix:
+            return []
+        result = []
+        max_level = m - 1
+        # Rows 0, 1, 2 -> 0, 2, 1
+        # 0, 1, 2, 3 -> 0, 3, 1, 2
+
+        row_start, row_end = 0, m-1
+        col_start, col_end = 0, n-1
+
+        while len(result) < m * n:
+            # l -> r
+            for j in range(col_start, col_end+1):
+                result.append(matrix[row_start][j])
+            row_start += 1
+            # r -> down
+            for i in range(row_start, row_end+1):
+                result.append(matrix[i][col_end])
+            col_end -= 1
+            # for j in range(col_end, col_start-1, -1):
+            #     result.append(matrix[row_end][j])
+            # row_end -= 1
+            # # print(result)
+            # for i in range(row_end, row_start-1, -1):
+            #     result.append(matrix[i][col_start])
+            # col_start += 1
+
+            # down -> l1
+            if row_start <= row_end:
+                for j in range(col_end, col_start-1, -1):
+                    result.append(matrix[row_end][j])
+                row_end -= 1
+
+            # l1 -> up
+            if col_start <= col_end:
+                for i in range(row_end, row_start-1, -1):
+                    result.append(matrix[i][col_start])
+                col_start += 1
+        return result
