f232a761de12509aadebde30e0d1b4bd8a76aafd
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-11-18 19:31:21 -0500

    Time: 1 ms (93.89%), Space: 18.4 MB (11.96%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -5,18 +5,18 @@
 from collections import deque
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(city):
-            for neighbor, connected in enumerate(isConnected[city]):
-                if connected == 1 and neighbor not in visited:
-                    visited.add(neighbor)
-                    dfs(neighbor)
+        def dfs(node):
+            for neigh, connected in enumerate(isConnected[node]):
+                if connected and (neigh not in visited):
+                    visited.add(neigh)
+                    dfs(neigh)
 
-        n_provinces = 0
+        num_provinces = 0
         visited = set()
         for city in range(len(isConnected)):
             if city not in visited:
                 visited.add(city)
                 dfs(city)
-                n_provinces += 1
+                num_provinces += 1
 
-        return n_provinces
+        return num_provinces
