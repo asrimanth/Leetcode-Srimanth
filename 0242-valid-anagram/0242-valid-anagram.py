1abe5a026cc3523e24f5e0a50a48373e5e61b97c
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2024-12-30 20:27:41 -0500

    Time: 26 ms (5.22%), Space: 18.4 MB (5.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,8 @@
-# Time Complexity: O(len(t))
-# Space Complexity: O(len(t))
-# Since 1 <= s.length, t.length <= 5 * 10^4 is a given constraint
 from collections import Counter
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # Count sort
-        return Counter(s) == Counter(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = Counter(sorted(s))
+        t_count = Counter(sorted(t))
+
+        return s_count == t_count
