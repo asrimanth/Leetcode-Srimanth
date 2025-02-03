0d3790cc42ccb1a00879c817a1527b9e14130236
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-02-02 21:08:23 -0500

    Time: 3 ms (51.94%), Space: 17.7 MB (47.33%) - LeetHub

---
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        max_inc, max_dec = 0, 0
+        N = len(nums)
+        # Strictly increasing
+        inc_seq_lens = []
+        for i in range(N):
+            prev = nums[i-1] if i-1 >= 0 else None
+            prev, curr = nums[i-1], nums[i]
+            if prev is not None and len(inc_seq_lens) and curr > prev:
+                inc_seq_lens[-1] += 1
+            else:
+                inc_seq_lens.append(1)
+
+        # Strictly decreasing
+        dec_seq_lens = []
+        for i in range(N):
+            prev = nums[i-1] if i-1 >= 0 else None
+            prev, curr = nums[i-1], nums[i]
+            if prev is not None and len(dec_seq_lens) and curr < prev:
+                dec_seq_lens[-1] += 1
+            else:
+                dec_seq_lens.append(1)
+
+        # print(inc_seq_lens, dec_seq_lens)
+
+        return max(max(inc_seq_lens), max(dec_seq_lens))
