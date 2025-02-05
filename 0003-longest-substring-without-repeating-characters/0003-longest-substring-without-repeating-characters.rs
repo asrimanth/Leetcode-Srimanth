1455d2f26a3db4af3e7dc8eba9d908b9d6adf8de
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-02-05 00:14:52 -0500

    Time: 1 ms (60.21%), Space: 2.3 MB (65.97%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+use std::cmp::max;
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut chars_idx_map: HashMap<char, usize> = HashMap::new();
+        let mut left: i32 = -1;
+        let mut result = 0;
+
+        for (right, ch) in s.chars().enumerate() {
+            match chars_idx_map.get(&ch) {
+                Some(idx) => left = max(left, *idx as i32),
+                None => (),
+            };
+
+            result = max(result, right as i32 - left);
+            chars_idx_map.insert(ch, right);
+        }
+
+        result
+    }
+}
