624f6dbd26b0507392e20a0491595d40d17c27a2
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-02-04 20:28:23 -0500

    Time: 3 ms (13.82%), Space: 17.9 MB (27.73%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,8 +1,35 @@
-# Time Complexity: O(N)
-# Space Complexity: O(N)
+
 from collections import Counter
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        return self.optimized_approach(s1, s2)
+    
+    def optimized_approach(self, s1: str, s2: str) -> bool:
+        """
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        """
+        ptr_1, ptr_2 = -1, -1
+        n = len(s1)
+        diff_count = 0
+
+        for i in range(n):
+            # Check diff
+            if s1[i] != s2[i]:
+                diff_count += 1
+                if ptr_1 == -1: ptr_1 = i
+                elif ptr_2 == -1: ptr_2 = i
+        if diff_count == 0: # No modification
+            return True
+        elif diff_count == 2 and s1[ptr_2] == s2[ptr_1] and s1[ptr_1] == s2[ptr_2]:
+            return True
+        return False
+    
+    def simple_approach(self, s1: str, s2: str) -> bool:
+        """
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+        """
         if s1 == s2:
             return True
         n = len(s1)
