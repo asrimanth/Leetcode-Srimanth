d78d11cd373be31904738b5223aeaa152196a9bb
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-02-09 08:18:15 +0700

    Time: 15 ms (10.8%), Space: 21.1 MB (41.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,21 @@
-# Time Complexity: O(N*log(N))
-# Space Complexity: O(N) for result
-
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # Sort intervals by start time
-        intervals = sorted(intervals, key=lambda x: x[0])
-        result = [intervals[0]]
-        for i in range(1, len(intervals)):
-            prev_interval = result[-1]
-            curr_interval = intervals[i]
-            if prev_interval[1] >= curr_interval[0]:
-                start = min(prev_interval[0], curr_interval[0])
-                end = max(prev_interval[1], curr_interval[1])
-                result[-1] = [start, end]
+        # Sort by start value
+        intervals = sorted(intervals, key= lambda x: x[0])
+        n = len(intervals)
+        i = 1
+        prev = intervals[0]
+        while i < n:
+            # cases:
+            # 1. start is between prev start and prev end
+            # 2. start is after prev end
+            old_start, old_end = intervals[i-1]
+            start, end = intervals[i]
+            if old_start <= start <= old_end:
+                new_val = [min(old_start, start), max(old_end, end)]
+                intervals[i-1] = new_val
+                del intervals[i]
+                n -= 1
             else:
-                result.append(curr_interval)
-        return result
+                i += 1
+        return intervals
