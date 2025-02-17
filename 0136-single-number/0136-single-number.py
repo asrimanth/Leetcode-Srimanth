d48e1280062af11f0516183761aca70fd08330f1
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-02-17 13:13:25 +0700

    Time: 0 ms (100%), Space: 19.6 MB (44.83%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
         # Use XOR
-        # Explanation: https://leetcode.com/problems/single-number/discuss/2778969/Python-XOR-Solution-Fully-Explained-(O(n)-time-O(1)-space)
         result = 0
         for num in nums:
-            result ^= num
+            result = result ^ num
         return result
