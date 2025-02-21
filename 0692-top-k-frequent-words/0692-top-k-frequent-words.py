6a7e111ffae4f4c19e286966bf27099feb98d52c
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-02-22 05:06:31 +0700

    Time: 3 ms (53.25%), Space: 18 MB (20.34%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,7 +1,31 @@
-from collections import Counter
+from collections import Counter, defaultdict
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        return self.solution_1(words, k)
+        return self.solution_2(words, k)
+    
+    def solution_2(self, words: List[str], k: int) -> List[str]:
+        # Bucket sort solution
+        data = Counter(words)
+        bucket = {i: [] for i in range(len(words))}
+        for key, count in data.items():
+            bucket[count].append(key)
+        
+        # Now sort the keys in the bucket lexicographically
+        for count in bucket.keys():
+            bucket[count].sort(reverse=True)
+            # Reverse lexicographic since we will pop from last.
+
+        result = []
+        # Now query the bucket in reverse order
+        for count in reversed(bucket.keys()):
+            temp = len(bucket[count])
+            while temp:
+                result.append(bucket[count].pop())
+                temp -= 1
+                k -= 1
+                if not k:
+                    return result
+
 
     def solution_1(self, words: List[str], k: int) -> List[str]:
         # A solution in O(nlogn) using heaps/sorting
