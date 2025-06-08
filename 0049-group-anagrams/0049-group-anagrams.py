76d28c5b3b89bfedbaf857ebf84324076451de6e
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-06-08 14:05:19 -0400

    Time: 21 ms (23.49%), Space: 22.9 MB (5.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        return self.group_by_sorted_str(strs)
+        return self.group_by_count(strs)
     
     def group_by_sorted_str(self, strs: List[str]) -> List[List[str]]:
         # Time Complexity: O(N*K*log(K))
@@ -10,3 +10,15 @@ class Solution:
         for data in strs:
             data_struct[tuple(sorted(data))].append(data)
         return list(data_struct.values())
+
+    def group_by_count(self, strs: List[str]) -> List[List[str]]:
+        # Time Complexity: O(N*K)
+        # Space Complexity: O(N*K)
+        result = defaultdict(list)
+        for data in strs:
+            count = [0] * 26
+            for char in data:
+                # Assume lower case
+                count[ord(char) - ord("a")] += 1
+            result[tuple(count)].append(data)
+        return list(result.values())
