61090a7f6d34ea669c96bfb7aac58675e331311b
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-08-01 17:33:36 -0400

    Time: 148 ms (7.87%), Space: 2.3 MB (31.49%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -1,24 +1,28 @@
-// Time Complexity: O(N)
+// Time Complexity: O(2 * N) = O(N)
 // Space Complexity: O(N)
-
 use std::collections::HashMap;
 use std::cmp::max;
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut chars_idx_map: HashMap<char, usize> = HashMap::new();
-        let mut left: i32 = -1;
+        let mut left = 0;
+        let mut right = 0;
+        let mut char_counts = HashMap::new();
         let mut result = 0;
+        // Using enumerate to iterate over chars with their indices
+        for (right, rchar) in s.chars().enumerate() {
+            // Get the count for the current character
+            let count = char_counts.entry(rchar).or_insert(0);
+            *count += 1;
 
-        for (right, ch) in s.chars().enumerate() {
-            match chars_idx_map.get(&ch) {
-                Some(idx) => left = max(left, *idx as i32),
-                None => (),
-            };
-
-            result = max(result, right as i32 - left);
-            chars_idx_map.insert(ch, right);
+            // While there is a duplicate character, shrink the window from the left
+            while *char_counts.get(&rchar).unwrap() > 1 {
+                // Get the character at the left pointer
+                let lchar = s.chars().nth(left).unwrap();
+                char_counts.entry(lchar).and_modify(|c| *c -= 1);
+                left += 1;
+            }
+            result = max(result, (right - left + 1) as i32);
         }
-
         result
     }
 }
