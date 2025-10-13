a9033e197b945535c4e75c4edae9fb5efc95831e
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-10-13 15:52:09 -0400

    Time: 0 ms (100%), Space: 18.9 MB (51.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,23 @@
-# Time Complexity: O(n)
-# Space Complexity: O(1)
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Hash map approach
-        hash_map = dict()
+        return self.hashmap_solution(nums, target)
+    
+    def hashmap_solution(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        hashmap = dict() # num:index is the mapping.
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+        return None
 
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in hash_map:
-                return (hash_map[complement], i)
-            hash_map[num] = i
+    def brute_force_solution(self, nums: List[int], target: int) -> List[int]:
+        # Time Complexity: O(N ** 2)
+        # Space Complexity: O(1)
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         return None
