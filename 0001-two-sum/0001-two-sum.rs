92a6fbd061eb927b678cb221d35ed834868262c2
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-10-13 16:20:16 -0400

    Time: 0 ms (100%), Space: 2.4 MB (38.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,17 +1,37 @@
-// Time Complexity: O(n)
-// Space Complexity: O(n)
 use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        // Hash Map approach
-        let mut hash_map = HashMap::new();
+        return Self::hash_map_solution(nums, target);
+    }
 
-         for (i, num) in nums.iter().enumerate() {
-            if let Some(&index) = hash_map.get(&(target - num)) {
-                return vec![index as i32, i as i32];
+    pub fn hash_map_solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // Time Complexity: O(N)
+        // Space Complexity: O(N)
+        let n: usize = nums.len();
+        let mut hashmap: HashMap<i32, i32> = HashMap::new();
+
+        for i in 0..n {
+            let (current, complement) = (nums[i], target - nums[i]);
+            if hashmap.contains_key(&complement){
+                let comp_index = hashmap[&complement];
+                return Vec::from([i as i32, comp_index as i32]);
             }
-            hash_map.insert(*num, i);
+            hashmap.entry(current).or_insert(i as i32);
         }
-        vec![] // Return an empty vector if no solution is found
+        return Vec::new();
+    }
+
+    pub fn brute_force_solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // Time Complexity: O(N ** 2)
+        // Space Complexity: O(1)
+        let n: usize = nums.len();
+        for i in 0..n {
+            for j in i+1..n{
+                if nums[i] + nums[j] == target {
+                    return Vec::from([i as i32, j as i32]);
+                }
+            }
+        }
+        return Vec::new();
     }
 }
