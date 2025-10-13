4600fc5107a3523d7f2253a149311ce0c34f8047
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-10-13 17:36:22 -0400

    Time: 15 ms (18.26%), Space: 17.9 MB (39.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,5 +1,43 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
+        return self.two_pointer(x)
+
+    def two_pointer(self, x: int) -> bool:
+        # Time Complexity: Number of digits in x = O(log10(x))
+        # Space Complexity: O(1)
+        if x < 0:
             return False
-        return str(x) == str(x)[::-1]
+        div = 1
+        while x > 10 * div:
+            div *= 10
+        # Two pointer approach -  div from left and mod from right
+        while x:
+            if x // div != x % 10:
+                return False
+            x = (x % div) // 10
+            div = div / 100
+        return True
+
+    def reverse_num(self, x: int) -> bool:
+        # Time Complexity: Number of digits in x = O(log10(x))
+        # Space Complexity: O(1)
+        if x < 0:
+            return False
+        temp = x
+        x_rev = 0
+        while temp:
+            digit = temp % 10
+            x_rev = x_rev*10 + digit
+            temp = temp // 10
+        if x == x_rev:
+            return True
+        else:
+            return False
+
+    def str_approach(self, x: int) -> bool:
+        # Time Complexity: Number of digits in x = O(log10(x))
+        # Space Complexity: O(1)
+        if x < 0:
+            return False
+        x = str(x)
+        return x == x[::-1]
