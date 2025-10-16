837e4eee050c670e14a9c249326f295946757c4e
Srimanth Agastyaraju <30816357+asrimanth@users.noreply.github.com>
2025-10-15 20:38:16 -0400

    Time: 2481 ms (5.87%), Space: 45.8 MB (40.12%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.py
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxIncreasingSubarrays(self, nums: list[int]) -> int:
+        # Time Complexity: O(N)
+        # Space Complexity: O(N)
         n = len(nums)
-        up, preUp, res = 1, 0, 0
+        prev, curr, result = 0, 1, 0
         for i in range(1, n):
+            print(prev, curr, result)
             if nums[i] > nums[i-1]:
-                up += 1
+                curr += 1
             else:
-                preUp = up
-                up = 1
-            half = up >> 1
-            m = min(preUp, up)
-            candidate = max(half, m)
-            if candidate > res:
-                res = candidate
-        return res
+                prev = curr
+                curr = 1
+            possibility_one = min(prev, curr) # Minimum of 2 subsequences
+            possibility_two = curr // 2 # Current sequence can be treated as 2 subsequences
+            result = max(result, max(possibility_one, possibility_two))
+        return result
